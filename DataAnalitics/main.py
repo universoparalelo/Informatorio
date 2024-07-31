fc56431b93d74b693f8980ba6485a2b9beb2d2b0
UniversoParalelo <cmartinrodich@gmail.com>
2024-07-31 17:48:34 -0300

    Sacando archivos

---
+++ DataAnalitics/main.py
@@ -0,0 +1,26 @@
+# from color50 import rgb, constants
+import os
+from datetime import (date, datetime, timedelta)
+
+# my_color = rgb(128, 0, 128)
+# another_color = rgb(40,40,66)
+# print(my_color + "Hello, World!" + constants.RESET)
+# print(another_color + "Hello, World!" + constants.RESET)
+
+# CHEQUEOS
+# CREATE --> HECHO
+# READ --> HECHO
+# UPDATE -->
+# DELETE -->
+
+fecha = date.fromisoformat("2024-06-07")
+
+print(fecha)
+
+fecha += timedelta(days=450)
+
+print(fecha)
+
+
+
+
